2de83a2e65e91a1df86f32fa88c8f724169389d6
Armando Dollia <dollia.armando@gmail.com>
2014-09-29 23:42:55 -0500

    sort

---
+++ Sorting/src/luckyNumber.java
@@ -0,0 +1,20 @@
+
+public final class luckyNumber {
+
+	private String name;
+	private Integer number;
+	
+	public luckyNumber(final String name, final Integer number){
+		this.name = name;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+	
+}
