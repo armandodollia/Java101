a26c0bb4d1a78c1e4d278c1181c737e15248af6e
Armando Dollia <dollia.armando@gmail.com>
2014-10-01 10:41:06 -0500

    class exercise

--- Lab4/src/lab4.java
+++ Lab4/src/lab4.java
@@ -2,37 +2,86 @@
  * Name: Armando Dollia*
  * Date: 09/23/2014	   *
  ***********************/
+import javax.swing.Box;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 public class lab4 {
 
 	public static void main(String[] args) 
 	{
 		String sex, output;
 		double weight, wrist, waist, hip, forearm, fat;
+		sex = JOptionPane.showInputDialog("Are you Male or Female?").toUpperCase().substring(0,1);
 
-		sex = JOptionPane.showInputDialog("Are you Male or Female?");
-		sex = sex.toUpperCase();
-		sex = sex.substring(0,1);
-
-		if(sex.compareTo("F") == 0)
+		if(sex.equals("F"))
 		{
-			weight = Double.parseDouble(JOptionPane.showInputDialog("Enter your weight"));
-			wrist = Double.parseDouble(JOptionPane.showInputDialog("Enter your wrist measurement at its fullest point"));
-			waist = Double.parseDouble(JOptionPane.showInputDialog("Enter your waist measurement at the navel"));
-			hip = Double.parseDouble(JOptionPane.showInputDialog("Enter your hip measurement at its fullest point"));
-			forearm = Double.parseDouble(JOptionPane.showInputDialog("Enter your forearm measurement at its fullest point"));
-				
+			//Create text fields and set width
+			JTextField weightField = new JTextField(5);
+			JTextField wristField = new JTextField(5);
+			JTextField waistField = new JTextField(5);
+			JTextField  hipField = new JTextField(5);
+			JTextField forearmField = new JTextField(5);
+			JPanel measurePanel = new JPanel(); //Create window and store it into an object
+			
+			measurePanel.add(new JLabel("Weight in lbs"));
+			measurePanel.add(weightField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			measurePanel.add(new JLabel("Wrist"));
+			measurePanel.add(wristField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			measurePanel.add(new JLabel("Waist"));
+			measurePanel.add(waistField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			measurePanel.add(new JLabel("Hip"));
+			measurePanel.add(hipField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			measurePanel.add(new JLabel("Forearm"));
+			measurePanel.add(forearmField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			
+			int result = JOptionPane.showConfirmDialog(null, measurePanel, 
+		               "Please Enter Measurements", JOptionPane.OK_CANCEL_OPTION);
+			
+			weight = Double.parseDouble(weightField.getText());
+			wrist = Double.parseDouble(wristField.getText());
+			waist = Double.parseDouble(waistField.getText());
+			hip = Double.parseDouble(hipField.getText());
+			forearm = Double.parseDouble(forearmField.getText());
+			//weight = Double.parseDouble(JOptionPane.showInputDialog("Enter your weight"));
+			//wrist = Double.parseDouble(JOptionPane.showInputDialog("Enter your wrist measurement at its fullest point"));
+			//waist = Double.parseDouble(JOptionPane.showInputDialog("Enter your waist measurement at the navel"));
+			//hip = Double.parseDouble(JOptionPane.showInputDialog("Enter your hip measurement at its fullest point"));
+			//forearm = Double.parseDouble(JOptionPane.showInputDialog("Enter your forearm measurement at its fullest point"));
+			
 			fat = ((weight-(((weight*.732)+8.987)+(wrist/3.14)-(waist*.157)-(hip*.249)+(forearm*.434)))*100)/weight;
 			output = String.format("Your body fat percentage is: %.2f%%", fat);
-			JOptionPane.showMessageDialog(null, output, "String Permutations", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, output, "Body Fat", JOptionPane.INFORMATION_MESSAGE);
 		}
 		else
 		{
-			weight = Double.parseDouble(JOptionPane.showInputDialog("Enter your weight"));
-			waist = Double.parseDouble(JOptionPane.showInputDialog("Enter your waist measurement at the navel"));
+			JTextField weightField = new JTextField(5);
+			JTextField waistField = new JTextField(5);
+			JPanel measurePanel = new JPanel();
+			measurePanel.add(new JLabel("Weight in lbs"));
+			measurePanel.add(weightField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			measurePanel.add(new JLabel("Waist"));
+			measurePanel.add(waistField);
+			measurePanel.add(Box.createHorizontalStrut(5));
+			
+			int result = JOptionPane.showConfirmDialog(null, measurePanel, 
+		               "Please Enter Measurements", JOptionPane.OK_CANCEL_OPTION);
+			
+			weight = Double.parseDouble(weightField.getText());
+			waist = Double.parseDouble(waistField.getText());
+			
+			//weight = Double.parseDouble(JOptionPane.showInputDialog("Enter your weight"));
+			//waist = Double.parseDouble(JOptionPane.showInputDialog("Enter your waist measurement at the navel"));
 			fat = ((weight-(((weight*1.082)+94.42)-(waist*4.15)))*100)/weight;
 			output = String.format("Your body fat percentage is: %.2f%%", fat);
-			JOptionPane.showMessageDialog(null, output, "String Permutations", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, output, "Body Fat", JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 }
